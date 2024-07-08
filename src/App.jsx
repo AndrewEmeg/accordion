548d747d4781b7b55a2ce98b17553281e140af9c
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-07-08 15:37:03 -0400

    Started the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 // import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [finalBill, setFinalBill] = useState(
+    "This is where the bill should appear"
+  );
+  const handleOnChange = (e) => {
+    console.log(e.target.value);
+    setFinalBill("okay");
+  };
+  return (
+    <div>
+      <Bill onChange={handleOnChange} />
+      <h2>{finalBill}</h2>
+    </div>
+  );
+}
+
+function Bill({ onChange }) {
+  return (
+    <div>
+      <span>How much was the bill? </span>
+      <input onChange={onChange} />
+    </div>
+  );
+}
 
 export default App;
